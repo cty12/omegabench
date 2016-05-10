bd1f1bd6794ea2c3071351f059f1dc9c7e3dcd49
Tianyu Chen <chentianyu@outlook.com>
2016-05-10 19:51:03 +0800

    added compiling php

--- apache-httpd/download-compile-apache.sh
+++ apache-httpd/download-compile-apache.sh
@@ -2,7 +2,7 @@
 
 #
 # The below script is a fork of the benchmarking script used in the Crane Project (https://github.com/columbia/crane).
-# It is polished and improved and becomes part of our Omegabench Project. 
+# It is polished and improved and becomes part of our Omegabench Project.
 # Author: Tianyu Chen
 # Date: May 10, 2016
 # Organization: Tsinghua University
@@ -13,11 +13,11 @@
 #
 # 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 #
-# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other 
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other
 # materials provided with the distribution.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
 # CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
 # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
@@ -62,7 +62,7 @@ if [ ! -f apr-util-$APR_UTIL_VER.tar.gz ]; then
 fi
 
 # untar
-echo "${BOLD}Untarring the source file to ${GREEN}httpd-$HTTPD_VER/srclib... ${RESET}"
+echo "${BOLD}Untarring the Apache source file... ${RESET}"
 tar zxf "httpd-$HTTPD_VER.tar.gz"
 tar zxf "apr-$APR_VER.tar.gz" -C "$APP_DIR/httpd-$HTTPD_VER/srclib"
 mv "$APP_DIR/httpd-$HTTPD_VER/srclib/apr-$APR_VER" "$APP_DIR/httpd-$HTTPD_VER/srclib/apr"
@@ -90,3 +90,31 @@ if [ $? -eq 0 ]; then
     ls -l "$APP_DIR/install/bin"
 fi
 
+# download php code bash
+# change working dir
+cd $APP_DIR
+echo "${BOLD}Working dir changed to ${GREEN}`pwd`${RESET}"
+if [ ! -f php-$PHP_VER.tar.gz ]; then
+    wget http://hk1.php.net/get/php-$PHP_VER.tar.gz/from/this/mirror -O php-$PHP_VER.tar.gz
+fi
+
+# untar
+echo "${BOLD}Untarring the PHP source file... ${RESET}"
+tar xzf "php-$PHP_VER.tar.gz"
+
+# change working dir
+cd $APP_DIR/php-$PHP_VER
+echo "${BOLD}Working dir changed to ${GREEN}`pwd`${RESET}"
+# configure & build
+./configure \
+        --prefix=$APP_DIR/install-php \
+        --with-apxs2=$APP_DIR/install/bin/apxs \
+        --disable-dom \
+        --disable-simplexml
+
+make -j4
+make install
+
+if [ $? -eq 0 ]; then
+    echo "${BOLD}PHP compilation ${GREEN}DONE!! ${RESET} Check the install-php dir. "
+fi
